3cc4255fc29f0bfa6767f5c46e64aaf0a2e8a5ef
Nick West <nickwe@inmotionhosting.com>
2018-04-09 15:09:03 -0400

    second commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Clone of brick break game
+#
+# Nick West, 04/09/2018
+
+try:
+	import sys
+	import random
+	import math
+	import os
+	import getopt
+	import pygame
+	from socket import *
+	from pygame.locals import *
+except ImportError, err:
+	print "couldn't load module. %s" % (err)
+	sys.exit(2)
+
+def load_png(name):
+	""" load image and return image object """
+	fullname = os.path.join('data', name)
+	try:
+		image = pygame.image.load(fullname)
+		if image.get_alpha is None:
+			image = image.convert()
+		else:
+			image = image.convert_alpha()
+	except pygame.error, message:
+		print 'Cannot load image:', fullname
+		raise SystemExit, message
+	return image, image.get_rect()
+
+class Ball(pygame.sprite.Sprite):
+	""" ball object to control movement and collision """
+
+	#constructor
+	def __init__(self, (xy), vector):
+		pygame.sprite.Sprite.__init__(self)
+		self.image, self.rect = load_png('ball.png')
+		screen = pygame.display.get_surface()
+		self.area = screen.get_rect()
+		self.vector = vector
+		self.hit = 0
